95e1ed68a52e1b47166bd39220b933dd06341d9e
youness <younessbouydia@gmail.com>
2024-08-04 14:25:21 +0100

    testing idb package

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 import { openDB } from 'idb';
-
+type TextItem = {
+  id?: number;
+  text: string;
+  timestamp: Date;
+};
 const dbName = 'MyAppDatabase';
 const storeName = 'textStore';
 
@@ -28,12 +33,42 @@ export async function getAllTexts() {
   return db.getAll(storeName)
 }
 const page = () => {
-    
+
+    const [textsList, setTextsList] = useState<TextItem[]>([]);
+  const [newText, setNewText] = useState('');
+
+  useEffect(() => {
+    loadTexts();
+  }, []);
+
+  async function loadTexts() {
+    const allTexts = await getAllTexts();
+    setTextsList(allTexts)
+  }
+
+  async function addTextBTN() {
+    await addText(newText)
+      setNewText('')
+    loadTexts()
+  }
+
   return (
     <div>
-        
-        
-        
+      <div>
+        <input
+          type="text"
+          value={newText}
+          onChange={e => setNewText(e.target.value)}
+        />
+        <button onClick={addTextBTN}>Add Text</button>
+      </div>
+      <ul>
+        {textsList.map(item => (
+          <li key={item.id}>
+            {item.text}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
